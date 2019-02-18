fb2715917cc66a819f1bbae94d3999dd11480f9f
osma <okovaskainen@evo.paivola.fi>
2019-02-18 22:30:44 +0200

    all the things

---
+++ utils/config.js
@@ -0,0 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+let PORT = process.env.PORT
+let MONGODB_URI = process.env.MONGODB_URI
+
+if (process.env.NODE_ENV === 'test') {  mongoUrl = process.env.TEST_MONGODB_URI}
+module.exports = {
+  PORT,
+  MONGODB_URI,
+}
